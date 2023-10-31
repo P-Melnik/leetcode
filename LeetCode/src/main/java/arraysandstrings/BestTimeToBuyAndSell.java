bd18387f6ea6a4f1f48cb159ab7457225d4c9627
Pavel Melnik <melnikpa91@gmail.com>
2023-10-31 23:59:11 +0400

    best time ti byu abd sell resolved

--- LeetCode/src/main/java/arraysandstrings/BestTimeToBuyAndSell.java
+++ LeetCode/src/main/java/arraysandstrings/BestTimeToBuyAndSell.java
@@ -1,5 +1,24 @@
 package arraysandstrings;
 
+/* You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+Example 2:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transactions are done and the max profit = 0. */
 public class BestTimeToBuyAndSell {
 
     public static void main(String[] args) {
@@ -9,22 +28,19 @@ public class BestTimeToBuyAndSell {
     }
 
     public static int maxProfit(int[] prices) {
-        int profit = 0;
-        int buying = 10001;
-        int selling = -1;
+        int buy = Integer.MAX_VALUE;
+        int benefit = 0;
         for (int i = 0; i < prices.length; i++) {
-            if (prices[i] < buying) {
-                buying = prices[i];
+            if (prices[i] < buy) {
+                buy = prices[i];
             } else continue;
-            for (int j = i; j < prices.length; j++) {
-                if (prices[j] > buying) {
-                    selling = prices[j];
-                    if (selling - buying > profit) {
-                        profit = selling - buying;
-                    }
+            for (int j = i + 1; j < prices.length; j++) {
+                if (prices[j] - buy > benefit) {
+                    benefit = prices[j] - buy;
                 }
             }
-        } return profit;
+        }
+        return benefit;
     }
 
 }
---
+++ LeetCode/src/main/java/sandbox/SandBox.java
@@ -0,0 +1,9 @@
+package sandbox;
+
+public class SandBox {
+    public static void main(String[] args) {
+
+    }
+
+
+}
